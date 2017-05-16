8b8943d26c0893af02fef519b1f88d55cda8a5af
Damien Le Thiec <damien.lethiec@gmail.com>
2017-05-16 09:53:53 +0200

    index method

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: "L'esprit Tchai", address: "Rue Oberkampf", stars: 2)
+Restaurant.create(name: "Big Mama", address: "Boulevard Richard Lenoir", stars: 5)
+Restaurant.create(name: "Mama Romma", address: "Rue Oberkampf", stars: 1)
