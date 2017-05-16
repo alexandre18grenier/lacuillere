4dcca3918d28482baf08e8ae4610c451c89c9405
Damien Le Thiec <damien.lethiec@gmail.com>
2017-05-16 09:44:38 +0200

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants
+
 
   # Afficher tous les restaurants
-  get 'restaurants', to: 'restaurants#index'
-  # Afficher un restaurant
-  get 'restaurants/:id', to: 'restaurants#show'
-  # Ajouter un restaurant
-  # Updater un restaurant
-  # Détruire un restaurant
+  # get 'restaurants', to: 'restaurants#index'
+  # # Afficher un restaurant
+  # get 'restaurants/:id', to: 'restaurants#show'
+  # # Ajouter un restaurant
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+  # # Updater un restaurant
+  # get 'restaurants/:id/edit', to: 'restaurants#edit'
+  # patch 'restaurants/:id', to: 'restaurants#update'
+  # # Détruire un restaurant
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
 end
