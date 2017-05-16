1b171f9cbccaa62166ce499a2d8d87dcbe9e9e1f
Damien Le Thiec <damien.lethiec@gmail.com>
2017-05-16 10:18:35 +0200

    new and create methods

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,15 +3,25 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
   def show
+    @restaurant = Restaurant.find(params["id"])
   end
   def new
+    @restaurant = Restaurant.new
   end
   def create
+    restaurant = Restaurant.create(restaurant_params)
+    redirect_to restaurant_path(restaurant)
   end
   def edit
   end
   def update
   end
   def destroy
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :stars)
   end
 end
