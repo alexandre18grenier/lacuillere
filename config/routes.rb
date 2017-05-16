873fd5eaec85e0ed2f848d66fcf83bf82bdf56eb
Damien Le Thiec <damien.lethiec@gmail.com>
2017-05-16 09:30:46 +0200

    add index and show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Afficher tous les restaurants
+  get 'restaurants', to: 'restaurants#index'
+  # Afficher un restaurant
+  get 'restaurants/:id', to: 'restaurants#show'
+  # Ajouter un restaurant
+  # Updater un restaurant
+  # Détruire un restaurant
 end
